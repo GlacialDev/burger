e6b355afed1c90a159d597f0d1094d21a94dde21
Klimenko Ivan <kli-van@yandex.ru>
2017-08-22 02:21:25 +0300

    реализован one-page-scroll

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	var menuOn = $('.header__hamburger-link'),
 		menuOff = $('.header__close-link'),
-		menu = $('.section--hamburger-menu');
+		menu = $('.hamburger-menu');
 
 	//гамбургер-меню для мобильных устройств в секции section__first-screen
 	$(menuOff).on('click', function(e){
@@ -42,10 +42,8 @@ $(document).ready(function() {
 	});
 
 	//подключение карты
-	ymaps.ready(init);
-	var myMap;
-	function init(){     
-		myMap = new ymaps.Map("map", {
+	function init(){
+	var	myMap = new ymaps.Map("map", {
 			center: [59.942037, 30.326865],
 			zoom: 12,
 			controls: []
@@ -57,8 +55,7 @@ $(document).ready(function() {
 		[59.972641, 30.311758], [59.946128, 30.386945], 
 		[59.893901, 30.317251], [59.916146, 30.493895]
 	],
-
-	myCollection = new ymaps.GeoObjectCollection({}, {
+		myCollection = new ymaps.GeoObjectCollection({}, {
 		draggable: false,
 		iconLayout: 'default#image',  //все метки - картинки
 		iconImageHref: 'img/section__contacts/map-marker.png',
@@ -71,5 +68,68 @@ $(document).ready(function() {
 	}
 	myMap.geoObjects.add(myCollection);
 	}
+	ymaps.ready(init);
 
+	//one-page-scroll
+	$(function() {
+		var screen = $('.one-page-scroll'),
+			firstSection = $('.section--first-screen').index(),
+			lastSection = $('section').length - 1,
+			screenPositionNow = 0;
+			scrolling = false;
+
+		var relocate = function (sectionPosition) { //функция смещения на следующую секцию
+
+			if(!scrolling) { //если уже идет скролл, нельзя еще раз скроллить
+				scrolling = true;
+
+				var position = (sectionPosition * -100) + '%';
+
+				screen.css({
+					'-webkit-transform:' : 'translateY(' + position + ')',
+					'-ms-transform:' : 'translateY(' + position + ')',
+					'transform' : 'translateY(' + position + ')',
+
+				});
+
+				setTimeout(function() {
+					scrolling = false;
+					$('.right-nav__item').eq(sectionPosition).addClass('active')
+						.siblings().removeClass('active');
+				}, 1300);
+			}
+		}
+
+		$('.wrapper').on('wheel', function(e) { //функция слежения за направлением движения колесика
+
+		var	screenNum = screenPositionNow, //внутренняя переменная (аналог внешней screenPositionNow)
+			deltaY = e.originalEvent.deltaY; //отслеживание движения колесика мышки (вверх/вниз)
+
+			if(!scrolling) { //если уже идет скролл, screenNum нельзя изменить
+				if (deltaY < 0 & screenNum > firstSection) {
+					//листаем вверх, если мы не на первой секции
+					screenNum--;
+				};
+
+				if (deltaY > 0 & screenNum < lastSection) { 
+					//листаем вниз, если мы не на последней секции
+					screenNum++;
+				};
+			}
+
+			screenPositionNow = screenNum; //изменение значения внешней переменной
+
+			relocate(screenPositionNow);
+		});
+
+		$('[data-scroll]').on('click', function(e) { // нав-меню точками справа
+			e.preventDefault();
+
+			var item = e.target;
+
+			screenPositionNow = $(item).attr('data-scroll')
+
+			relocate(screenPositionNow);
+		});
+	});
 });
